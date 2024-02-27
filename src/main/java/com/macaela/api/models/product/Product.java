d5de708a1a46915ac69ef1ba2517e1f27870792e
Luis Leal <95981661+luislealhdz@users.noreply.github.com>
2024-02-26 20:30:35 -0600

    Product model creation

--- src/main/java/com/macaela/api/models/product/Product.java
+++ src/main/java/com/macaela/api/models/product/Product.java
@@ -1,5 +1,51 @@
 package com.macaela.api.models.product;
 
-public class Product {
+import com.macaela.api.models.user.User;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "product")
+@Entity(name = "Product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Product_id")
+    private Long id;
+
+    @Column(name = "Product_category")
+    private String category;
+
+    @Column(name = "Product_color")
+    private String color;
+
+    @Column(name = "Product_size")
+    private String size;
+
+    @Column(name = "Product_stock")
+    private int stock;
+
+    @Column(name = "Product_price")
+    private double price;
+
+    @Column(name = "Product_description")
+    private String description;
+
+    @Column(name = "Product_image")
+    private String image;
+
+    @Column(name = "users_User_id")
+    private User userId;
 }
