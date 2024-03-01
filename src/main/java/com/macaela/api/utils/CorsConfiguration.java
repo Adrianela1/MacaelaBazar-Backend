efcffab4e3da4a3faa64ccf0032889ef357380ff
Adrian Nevarez <124970237+Adrianela1@users.noreply.github.com>
2024-03-01 11:04:30 -0600

    modificamos el cors

--- src/main/java/com/macaela/api/utils/CorsConfiguration.java
+++ src/main/java/com/macaela/api/utils/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://macaelabazar-backend-byr0.onrender.com")
+            .allowedOrigins("http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
